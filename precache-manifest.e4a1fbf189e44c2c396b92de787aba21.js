af55fd66101570eadc797f3dd94d348ec287ffff
Krzysztof Baum <baumkrzysztof@gmail.com>
2019-12-27 15:31:08 +0100

    Updates

---
+++ precache-manifest.e4a1fbf189e44c2c396b92de787aba21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb831a579070cbef971dc2bc20fd3c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ec987c1270b7136d79c",
+    "url": "/static/css/main.6b340c2e.chunk.css"
+  },
+  {
+    "revision": "c92df70e3b89ea2aac3f",
+    "url": "/static/js/2.ecae338d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ecae338d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ec987c1270b7136d79c",
+    "url": "/static/js/main.10342522.chunk.js"
+  },
+  {
+    "revision": "c842c055cd4f1b70441b",
+    "url": "/static/js/runtime-main.0de59fe5.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/static/media/header3.d9d36b65.jpg"
+  }
+]);
